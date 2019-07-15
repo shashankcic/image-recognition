c47744dd284268e3623876e82501af946fcb3290
Shashank Singh <singhshashankcic@ducic.ac.in>
2019-07-15 23:24:08 +0530

    Updates

---
+++ precache-manifest.3af24a4b93cbe74df5c90d12775c0736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3249646b1c458e67cae696cc25b21eae",
+    "url": "/image-recognition/index.html"
+  },
+  {
+    "revision": "b869361a09aee3c0a336",
+    "url": "/image-recognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c795eefc676798e56588",
+    "url": "/image-recognition/static/css/main.c2097675.chunk.css"
+  },
+  {
+    "revision": "b869361a09aee3c0a336",
+    "url": "/image-recognition/static/js/2.a24f987a.chunk.js"
+  },
+  {
+    "revision": "c795eefc676798e56588",
+    "url": "/image-recognition/static/js/main.05a82fe8.chunk.js"
+  },
+  {
+    "revision": "da236e1df4f0408c52b8",
+    "url": "/image-recognition/static/js/runtime~main.1bab8aa3.js"
+  }
+]);
